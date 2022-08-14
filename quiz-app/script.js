59b2d2e7186666bd7aa77aa327b0b56a63684a25
aburizalb <muhammadaburizalbakri@gmail.com>
2022-08-14 13:33:58 +0800

    3

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -23,8 +23,18 @@ const quizData = [
 		d: "Ir. Soekarno",
 		benar: "d",
 	},
+	{
+		question: "Apa Kabarmu?",
+		a: "baik",
+		b: "sakit",
+		c: "tidak baik",
+		d: "buruk",
+		benar: "a",
+	},
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionX = document.getElementById("question");
 const a_txt = document.getElementById("a_txt");
 const b_txt = document.getElementById("b_txt");
@@ -33,10 +43,13 @@ const d_txt = document.getElementById("d_txt");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+	deselected();
+
 	const currentQuizData = quizData[currentQuiz];
 
 	questionX.innerText = currentQuizData.question;
@@ -44,17 +57,38 @@ function loadQuiz() {
 	b_txt.innerText = currentQuizData.b;
 	c_txt.innerText = currentQuizData.c;
 	d_txt.innerText = currentQuizData.d;
+}
 
-	currentQuestion++;
+function getSelected() {
+	let answer = undefined;
+
+	answerEls.forEach((answerEl) => {
+		if (answerEl.checked) {
+			answer = answerEl.id;
+		}
+	});
+	return answer;
+}
+
+function deselected() {
+	answerEls.forEach((answerEl) => {
+		answerEl.checked = false;
+	});
 }
 
 submitBtn.addEventListener("click", () => {
-    currentQuiz++;
+	const answer = getSelected();
 
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        //todo show result
-        alert("Soal habis");
-    }
+	if (answer) {
+		if (answer === quizData[currentQuiz].benar) {
+			score++;
+		}
+		currentQuiz++;
+		if (currentQuiz < quizData.length) {
+			loadQuiz();
+		} else {
+			//todo show result
+			quiz.innerHTML = `<h2>Hasil jawaban benar adalah ${score}/${quizData.length} Pertanyaan.</h2><button onclick="location.reload()">Reload</button>`;
+		}
+	}
 });
